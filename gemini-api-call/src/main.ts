f73e0047cd658c37c4663e9d14f6f9acda060678
peserra <pedro.occulate@gmail.com>
2025-05-14 08:56:09 -0300

    pequenas alteracoes

--- gemini-api-call/src/main.ts
+++ gemini-api-call/src/main.ts
@@ -3,9 +3,16 @@ import { FileTextModule } from "./modules/FileTextModule";
 import * as fs from "fs";
 import * as path from "path";
 
-const filePath = path.resolve(__dirname, "..", "Files", "file.txt");
+async function main() {
+	const filePaths: string[] = [
+		path.resolve(__dirname, "..", "Files", "SubstituicaoVegana.pdf")
+	];
+	const pdfContents = await FileTextModule.readPdfFilesToStrings(filePaths);
+	const outputPath = path.resolve(__dirname, "..", "Files", "SubstituicaoVegana.txt");
+	fs.writeFileSync(outputPath, pdfContents.join("\n\n"), "utf-8");
+}
 
-const fileContent = fs.readFileSync(filePath, "utf-8");
+main();
 
 // vai estar no BD do usuario:
 const apiKey: string = "AIzaSyA941xEF5AccsmYtflct4hv8XNfLp0EQQE";
@@ -13,31 +20,28 @@ const apiKey: string = "AIzaSyA941xEF5AccsmYtflct4hv8XNfLp0EQQE";
 const apiCall = new GoogleApiCall(apiKey);
 const content: string = "quais os 5 primeiros artigos da declaração dos direitos humanos?";
 
+// async function processContent(prompt: string) {
+// 	try {
+// 		const fileChuncks = await FileTextModule.splitContentIntoChunks(fileContent, 300);
+// 		const embbedings = await getContentEmbbedingList(fileChuncks);
+// 		const index = await apiCall.calculateRetrievalQuery(embbedings, prompt)
+// 		const answer = fileChuncks[index];
+// 		const answerPrompt = `Reescreva o texto ${answer}, de modo a me dar apenas a resposta que eu busquei na
+// 		pergunta original '${prompt}'.Essa resposta deve estar didática, como um professor explicando de maneira acessível. Casoalgum nome próprio seja incluído na resposta, ele não deve estar na resposta final.`
+// 		const response = await apiCall.generateSimpleAnswer(answerPrompt);
+// 		console.log(response);
+// 	} catch (error) {
+// 		console.error(error);
+// 	 }
+// }
 
-async function processContent(prompt: string) {
-	try {
-		const fileChuncks = await FileTextModule.splitContentIntoChunks(fileContent, 300);
-		const embbedings = await getContentEmbbedingList(fileChuncks);
-		const index = await apiCall.calculateRetrievalQuery(embbedings, prompt)
-		const answer = fileChuncks[index];
-		const answerPrompt = `Reescreva o texto ${answer}, de modo a me dar apenas a resposta que eu busquei na
-		pergunta original '${prompt}'.Essa resposta deve estar didática, como um professor explicando de maneira acessível. Casoalgum nome próprio seja incluído na resposta, ele não deve estar na resposta final.`
-		const response = await apiCall.generateSimpleAnswer(answerPrompt);
-		console.log(response);
-	} catch (error) {
-		console.error(error);
-	}
-}
+// async function getContentEmbbedingList(fileContentChunks: string[]): Promise<number[][]> {
+// 	const embeddingList: number[][] = [];
+// 	for (const chunk of fileContentChunks) {
+// 		const embedding = await apiCall.generateEmbeddings(chunk);
+// 		embeddingList.push(embedding);
+// 	}
+// 	return embeddingList;
+// }
 
-
-async function getContentEmbbedingList(fileContentChunks: string[]): Promise<number[][]> {
-	const embeddingList: number[][] = [];
-	for (const chunk of fileContentChunks) {
-		const embedding = await apiCall.generateEmbeddings(chunk);
-		embeddingList.push(embedding);
-	}
-	return embeddingList;
-}
-
-
-processContent(content);
+// processContent(content);
--- gemini-api-call/src/modules/FileTextModule.ts
+++ gemini-api-call/src/modules/FileTextModule.ts
@@ -1,3 +1,8 @@
+import { error } from "console";
+import * as fs from "fs";
+import * as path from "path";
+import pdfParse from "pdf-parse";
+
 export class FileTextModule {
   static async splitContentIntoChunks(content: string, chunkSize: number): Promise<string[]> {
     const chunks: string[] = [];
@@ -5,5 +10,22 @@ export class FileTextModule {
       chunks.push(content.slice(i, i + chunkSize));
     }
     return chunks;
+  }
+
+  static async readPdfFilesToStrings(pdfFilePaths: string[]): Promise<string[]> {
+    console.log(`${pdfFilePaths}`)
+    const results: string[] = [];
+
+    for (const filePath of pdfFilePaths) {
+      try {
+        const file = fs.readFileSync(filePath);
+        const data = await pdfParse(file);
+        console.log(data.text);
+        results.push(data.text);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    return results;
   }
 }
